e3909e245969a778b14b8acdb5a5ae049b410c3e
Frederic MAMATH <frederic.mamath@gmail.com>
2023-08-29 08:43:39 +0200

    :package: Adds react-intl

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { IntlProvider, FormattedMessage } from "react-intl";
+
+import enTranslation from "./translations/en.json";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <IntlProvider messages={enTranslation} locale="en" defaultLocale="en">
+      <div>
+        <FormattedMessage id="technicalWorkshop.frontend.title" />
+      </div>
+      <div>
+        <FormattedMessage id="technicalWorkshop.title" />
+      </div>
+    </IntlProvider>
+  );
 };
 
 export default App;
